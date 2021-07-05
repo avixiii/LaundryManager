ae2d715601c1feab981e883c8142b1805b42ff4c
Phạm Anh Tuấn <tuanpham5024@gmail.com>
2021-07-05 21:20:29 +0700

    Delete UC UCCreateBill

--- LaundryManager/Views/UCInvoice.cs
+++ LaundryManager/Views/UCInvoice.cs
@@ -20,25 +20,8 @@ namespace LaundryManager.Views
 
         private void btnCreateInvoice_Click(object sender, EventArgs e)
         {
-            bool flag = true;
-
-            gcInvoice.Dock = DockStyle.Bottom;
-
-            if (flag == true)
-            {
-                panelCreateBill.Controls.Clear();
-                UCCreateBill ucCreateBill = new UCCreateBill();
-                
-                ucCreateBill.Height = 600;
-                panelCreateBill.Dock = DockStyle.Top;
-                gcInvoice.Height = gcInvoice.Height - 500;
-                panelCreateBill.Controls.Add(ucCreateBill);
-                btnCreateInvoice.Enabled = false;
-                btnEdit.Enabled = false;
-                btnPrint.Enabled = false;
-                btnIgnore.Enabled = true;
-                btnSave.Enabled = true;
-            }
+            fCreateBills fCreateBills = new fCreateBills();
+            fCreateBills.ShowDialog();
         }
 
         private void btnExit_Click(object sender, EventArgs e)
@@ -48,18 +31,10 @@ namespace LaundryManager.Views
 
         private void btnIgnore_Click(object sender, EventArgs e)
         {
-            panelCreateBill.Controls.Clear();
-            gcInvoice.Dock = DockStyle.Fill;
-            btnCreateInvoice.Enabled = true;
-            btnIgnore.Enabled = false;
-            btnEdit.Enabled = true;
-            btnPrint.Enabled = true;
-            btnSave.Enabled = false;
         }
 
         private void UCInvoice_Load(object sender, EventArgs e)
         {
-            btnIgnore.Enabled = false;
         }
     }
 }
